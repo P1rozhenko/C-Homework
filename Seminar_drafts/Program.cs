0203ccaeb2f691873cb799d46d5011b9083348ef
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-08-18 22:33:36 +0400

    homework seminar 3

---
+++ Seminar_drafts/Program.cs
@@ -0,0 +1,92 @@
+﻿
+// void method1()
+// {
+//     Console.WriteLine("Pira");
+// }
+// method1();
+
+// //type2
+
+// void method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// method2(msg: "Message texxt");
+
+//type2.1
+
+// void method2(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         count++;
+//     }
+// }
+// method21("Message texxt", 4);
+
+
+
+
+
+//Напишите программу, кот принимает 
+//число на вход число и выдает количество разрядов 
+
+// int razryadnost (int R)
+// {
+// //Console.WriteLine("Введите число");
+// //int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = 0;
+
+// while (R > 0)
+// {
+//     R /= 10;
+//     i++;
+// }
+// //Console.WriteLine(i);
+// return i;
+// }
+// int B = razryadnost (12345999);
+// Console.WriteLine(B);
+
+
+
+
+//Принимаем на вход N и выдаем произведение чисел от 1 до N
+
+// ulong fac(ulong R)
+
+// {
+//     ulong N = 1;
+//     ulong i = 1;
+//     while (i <= R)
+//     {
+//         N *= i;
+//         i++;
+//     }
+//     return N;
+// }
+// Console.WriteLine(fac(21));
+
+
+
+// Выводим массив из 8 чисел со случайными 0 и 1
+
+
+int[] M = { 2, 3, 4, 5, 6, 7, 8, 9 };
+int i = 0;
+
+while (i < 8)
+{
+    M[i] = new Random().Next(0, 2);
+    i++;
+}
+int i = 0;
+for (i, i < 8, i++)
+{
+    Console.Write(M[i]);
+}
+
+
---
+++ Z9/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Задача 21
+// Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты точки 1");
+
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки 2");
+
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double S = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+Console.WriteLine(S);
+
