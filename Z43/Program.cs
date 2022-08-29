e4e33d40e614dac5308467cd0299213d9cc843a0
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-08-29 16:53:24 +0400

    zad 41 and 43

---
+++ Z43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+int p1 = 0;
+int p2 = 0;
+int x = 1;
+
+p1 = k1 * x + b1;
+p2 = k2 * x + b2;
+
+Console.WriteLine($"({p1};{p2})");
