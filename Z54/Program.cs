0727f596e769f32ceb0563efe38d052acb5bb32e
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-09-18 12:36:45 +0400

    54, 56

---
+++ Z54/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+//убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] matrix = new int[3, 4];
+
+void fillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 9);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SelectionSort(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - j - 1; k++)
+            {
+                if (matrix[i,k] < matrix[i, k + 1])
+                {
+                    int t = matrix [i, k];
+                    matrix [i, k] = matrix [i, k + 1];
+                    matrix [i, k + 1] =  t;
+                }
+                
+            }
+        }
+        
+    }
+}
+fillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+SelectionSort(matrix);
+PrintArray(matrix);
---
+++ Z56/Program.cs
@@ -0,0 +1,118 @@
+﻿// // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая 
+// // будет находить строку с наименьшей суммой элементов.
+// // Например, задан массив:
+// // 1 4 7 2
+// // 5 9 2 3
+// // 8 4 2 4
+// // 5 2 6 7
+// // Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+// // наименьшей суммой элементов: 1 строка
+
+// int[,] matrix = new int[3, 4];
+
+// void fillArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(0, 9);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FindLeast(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         int k = 0 //строка с наименьшим
+//         if ( Matrix[k])
+        
+//         Console.WriteLine();
+//     }
+// }
+
+
+// fillArray(matrix);
+// PrintArray(matrix);
+// Console.WriteLine();
+// SelectionSort(matrix);
+// PrintArray(matrix);
+
+
+
+void PrintArray(int[,] matr)
+
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write($"{matr[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m, n] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int Sum(int[,] matr)
+{
+    int sum = 0;
+    int minSum = 0;
+    int minNum = 0;
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+
+        {
+            if (m == 0) //если строка первая
+            {
+                sum += matr[m, n];
+                minSum += matr[m, n]; //по умолчанию считаем что сумма первой строки минимальная
+            }
+
+            else sum += matr[m, n]; //для всех остальных строк просто считаем сумму
+        }
+
+        if (sum < minSum)
+        {
+            minSum = sum; 
+            minNum = m; 
+        }
+        sum = 0;         
+    }
+    return minNum;
+}
+
+int[,] matrix = new int[4, 4];
+
+FillArray(matrix);
+
+PrintArray(matrix);
+
+Console.WriteLine();
+
+Console.WriteLine(Sum(matrix));
