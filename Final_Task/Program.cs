6bf5d2cbee47d390b2155912a4ba94a588cf3e26
Evgeny Pirozhenko <pir@MacBook-Pro-Evgeny.local>
2022-09-21 20:28:28 +0400

    final task

---
+++ Final_Task/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+// Console.Write("Введите фразу ");
+// int [] arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+// int [] arr2 = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         if (arr Length < 3)
+//         {
+//             arr += arr2;
+//         }
+        
+//     }
+
+
+Console.Write("Введите фразу ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+int[] arr2 = {0};
+for (int i = 0; i < arr.Length; i++)
+{
+    if (i < 3)
+    {
+        arr2 = i;
+    }
+}
+    
+//Console.WriteLine("{arr}");
+Console.WriteLine("{arr2");
+
+// string phrase = "The quick brown fox jumps over the lazy dog.";
+// string[] words = phrase.Split(' ');
